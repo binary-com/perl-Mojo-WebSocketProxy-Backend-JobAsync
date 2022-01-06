ef3d29079c55cd5fcde2e72ceeae3036ac0cee2f
chylli-binary <chylli@binary.com>
2022-01-06 21:26:45 +0800

    init

---
+++ lib/Mojo/WebSocketProxy/Backend/JobAsync.pm
@@ -0,0 +1,36 @@
+package Mojo::WebSocketProxy::Backend::JobAsync;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+Mojo::WebSocketProxy::Backend::JobAsync - Module abstract
+
+=head1 SYNOPSIS
+
+    use Mojo::WebSocketProxy::Backend::JobAsync;
+    my $instance = Mojo::WebSocketProxy::Backend::JobAsync->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
